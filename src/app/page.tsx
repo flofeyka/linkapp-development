35d8401968c0b98f05edd3522422a0868c0e4b27
Danil Bashirov <flofey@yandex.ru>
2024-08-22 21:12:20 +0500

    Adaptive design for phones added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,41 +10,42 @@ import Header from "@/components/Header";
 import { motion } from "framer-motion";
 
 export default function Home() {
-    return <div className={"px-[90px] pt-7"}>
+    return <div className={"px-[90px] sm:p-0 pt-7"}>
         <Header />
         <main>
-            <Card className={"p-10 h-full "}>
-                <div className="flex w-full">
-                    <div className={"relative h-full min-h-[65vh] w-full flex flex-col justify-between"}>
-                        <div className={"text-5xl max-w-[75%] leading-[3.5rem]"}>
+            <Card className={"p-10 sm:p-4 h-full "}>
+                <div className="flex w-full sm:flex-col">
+                    <div className={"relative h-full h-[65vh] sm:h-full w-full flex flex-col justify-between "}>
+                        <div className={"text-5xl sm:text-3xl max-w-[75%] sm:max-w-[100%] leading-[3.5rem]"}>
                             Успех начинается здесь: <span className={"text-primary-400 font-semibold"}>программный код</span>, созданный
                             для ваших
                             идей
                             и результатов
                         </div>
 
-                        <div className={"flex gap-3 h-full relative items-end"}>
+                        <div className={"flex gap-3 sm:gap-1 h-full sm:my-3 relative sm:items-center items-end"}>
                             <div
-                                className={"w-[15vw] h-[20vh] rounded-2xl p-5 bg-[#303238] gap-2 flex flex-col justify-between hover:mb-3 transition-all"}>
-                                <div className={"text-5xl"}>50+</div>
+                                className={"w-[15vw] sm:w-[100%] h-[20vh] sm:h-[15vh] sm:p-3 rounded-2xl p-5 bg-[#303238] gap-2 flex flex-col justify-between hover:mb-3 transition-all"}>
+                                <div className={"text-5xl sm:text-3xl"}>50+</div>
 
                                 <div className={"text-gray-400"}>выполненных кейсов</div>
                             </div>
                             <div
-                                className={"w-[15vw] h-[20vh] rounded-2xl p-5 bg-[#303238] gap-2 flex flex-col justify-between hover:mb-3 transition-all"}>
-                                <div className={"text-5xl"}>2+</div>
+                                className={"w-[15vw] sm:w-[50%]  h-[20vh] sm:h-[15vh] sm:p-3 rounded-2xl p-5 bg-[#303238] gap-2 flex flex-col justify-between hover:mb-3 transition-all"}>
+                                <div className={"text-5xl sm:text-3xl"}>2+</div>
 
                                 <div className={"text-gray-400"}>года опыта работы</div>
                             </div>
                         </div>
 
                     </div>
-                    <div className="relative ">
-                        <video className="rounded-3xl object-cover relative w-full" loop={true} muted={true} playsInline={true} autoPlay={true} src="/bg-landing.mov" />
+                    <div className="relative">
+                        <video className="rounded-3xl object-cover relative w-full sm:h-[37.5vh]" loop={true} muted={true} playsInline={true} autoPlay={true} src="/bg-landing.mov" />
                     </div>
                 </div>
             </Card>
 
+            <div className="sm:px-4">
             <div className={"mt-16 "}>
                 <div className={"text-5xl my-7 flex items-center justify-between"}>
                     <div>кейсы</div>
@@ -56,9 +57,7 @@ export default function Home() {
                 </div>
                 {/* <div className={"flex gap-3 flex-wrap "}>
                     {cases.map((item, index) => <CaseItem projectName={item.projectName} description={item.description} key={index} type={item.type} name={item.name} />)}
-
                 </div> */}
-
             </div>
 
             <div className="mt-16">
@@ -78,13 +77,13 @@ export default function Home() {
             </div>
 
             <div className="mt-16">
-                <div className="bg-[#202126] w-full p-7 rounded-3xl flex justify-between">
-                    <div className="text-5xl max-w-[40%] font-semibold"><span className="text-primary-400">оставьте заявку</span>, чтобы обсудить проект</div>
-                    <div className="flex flex-col gap-3 w-[50%]">
-                        <div className="flex gap-2"><Input size="lg" placeholder="Имя" /> <Input size="lg" placeholder="Телефон" /></div>
+                <div className="bg-[#202126] w-full p-7 sm:p-4 rounded-3xl flex sm:flex-col justify-between">
+                    <div className="text-5xl max-w-[40%] font-semibold sm:mb-3 sm:text-4xl sm:max-w-[100%]"><span className="text-primary-400">оставьте заявку</span>, чтобы обсудить проект</div>
+                    <div className="flex flex-col gap-3 sm:gap-2 w-[50%] sm:w-full">
+                        <div className="flex gap-2 sm:gap-2 sm:flex-col"><Input size="lg" placeholder="Имя" /> <Input size="lg" placeholder="Телефон" /></div>
                         <div><Input size="lg" placeholder="Почта" /></div>
                         <div><Textarea size="lg" placeholder="Комментарий" /></div>
-                        <div className="w-[50%] text-gray-400">В чем заключается задача? В какие сроки?
+                        <div className="w-[50%] sm:w-full sm:text-xs text-gray-400">В чем заключается задача? В какие сроки?
                             Несколько слов о проекте</div>
 
                         <div>
@@ -95,11 +94,13 @@ export default function Home() {
                             </Select>
                         </div>
                         <div>
-                            <Button size="lg">оставить заявку</Button>
+                            <Button size="lg" className="sm:w-full">оставить заявку</Button>
                         </div>
                     </div>
                 </div>
             </div>
+            </div>
+            
         </main>
         <footer>Footer</footer>
     </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,8 +2,11 @@ import { Button, Card } from "@nextui-org/react";
 import Link from "next/link";
 
 export default function Header() {
-    return <header className={"mb-3 flex w-full justify-between items-center font-semibold"}>
-        <Card className={"min-w-[210px] flex items-center p-4"}>
+    return <header className={"mb-3 flex w-full justify-between items-center font-semibold "}>
+        <Card className="lg:hidden md:hidden h-[7.5vh] w-screen flex justify-center items-end px-5">
+            Burger huynya
+        </Card>
+        <Card className={"min-w-[210px] flex items-center p-4 sm:hidden"}>
             <div className={"flex gap-5 text-[14px]"}>
                 <Link href="/" className={"hover:text-primary-400 transition-all"}>кейсы</Link>
                 <Link href="/" className={"hover:text-primary-400 transition-all"}>услуги</Link>
@@ -11,7 +14,7 @@ export default function Header() {
             </div>
         </Card>
 
-        <div>
+        <div className="sm:hidden">
             <Button className={"font-semibold"} size={"lg"}>оставить заявку</Button>
         </div>
     </header>
