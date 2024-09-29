c094ea27e2c4b624647e4507a98981378ac0a922
Danil Bashirov <flofey@yandex.ru>
2024-09-29 22:36:20 +0500

    Meta tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,30 @@
-import type {Metadata} from "next";
-import {Inter} from "next/font/google";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import ProvidersWrapper from "@/components/ProvidersWrapper";
 
-const inter = Inter({subsets: ["latin"]});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: "LinkApp Development",
-    description: "дванил дваширов самый ебейший порномист на свете",
+  title: "LinkApp Development - Веб-разработка веб-приложений любой сложности в кратчайшие сроки",
+  description:
+    "Компания LinkApp Development предлагает профессиональную веб-разработку веб-приложений любой сложности в самые короткие сроки. Узнайте больше о наших услугах и команде экспертов.",
 };
 
 export default function RootLayout({
-                                       children,
-                                   }: Readonly<{
-    children: React.ReactNode;
+  children,
+}: Readonly<{
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang="en">
-            <body className={inter.className}>
-                <ProvidersWrapper>
-                    <div className={"text-white bg-[#111] min-h-screen pb-10"}>
-                        {children}
-                    </div>
-                </ProvidersWrapper>
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ProvidersWrapper>
+          <div className={"text-white bg-[#111] min-h-screen pb-10"}>
+            {children}
+          </div>
+        </ProvidersWrapper>
+      </body>
+    </html>
+  );
 }
